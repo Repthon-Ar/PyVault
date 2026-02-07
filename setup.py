aa770680fbe8f4d991756059ad0689f0f0dc754d
Repthon-Ar <alfashymwswlyny@gmail.com>
2026-02-07 10:32:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PyVault",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=["requests", "cryptography", "telethon"],
     description="Encrypted media upload library without API"
